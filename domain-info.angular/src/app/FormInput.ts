a7312d35200d48ad4dda2d0dc675f61ce52c37de
Andrew Kress <andrew.kress@gmail.com>
2021-08-04 23:37:27 -0500

    refactoring

--- domain-info.angular/src/app/FormInput.ts
+++ domain-info.angular/src/app/FormInput.ts
@@ -1,9 +1,15 @@
 export class FormInput {
   domainOrIp: string;
   services: Array<string>;
+  generatedQuery: string;
+  selectedItemsList: Array<object>;
+  checkedIDs: Array<string>;
 
   constructor() {
     this.domainOrIp = "";
     this.services = [];
+    this.generatedQuery = "";
+    this.selectedItemsList = [];
+    this.checkedIDs = [];
   }
 }
--- domain-info.angular/src/app/query.service.ts
+++ domain-info.angular/src/app/query.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from "rxjs/operators";
-
-
-import { Query } from './query';
 
 @Injectable()
 export class QueryService {
@@ -18,11 +14,6 @@ export class QueryService {
       subQuery += "&services=" + service;
     });
     let query = "https://localhost:5001/query?domainOrIp=" + domainOrIp + subQuery;
-    console.log(query);
-    let results = this.http.get(query);
-    console.log(results);
-    return results;
-      //.pipe(map(res => res.json()))
-      //.catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+    return this.http.get(query);
   }
 }
